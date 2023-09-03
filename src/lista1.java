9a2a3170d859019040c90882a4b7d5d3b147ea3d
Bianca Moreira <bianca.engcomput23@gmail.com>
2023-09-03 18:50:10 -0300

    Resolução de questões
    Até a 7

--- src/lista1.java
+++ src/lista1.java
@@ -1,4 +1,6 @@
+
 public class lista1 {
+    
     public static void main (String[] args){
         /* Questão 1
         int   i = 150;
@@ -10,19 +12,112 @@ public class lista1 {
 
         /* Questão 2
         int i = 1;
-        int soma = 0;
+        int resultado = 0;
         while(i<=1000){
-            soma = soma + i;
+            resultado = resultado + i;
             i = i + 1;
         }
-        System.out.println(soma);
+        System.out.println(resultado);
         
         */
 
-        
-        for(int i = 3; i <= 100; i=i+3){
+         /*Questão 3 
+        for(int i = 3; i <= 100; i+=3){
             System.out.println(i);
+        }*/
+
+
+        /* Questão 4 
+        int resultado = 1;
+        System.out.println(resultado);
+        for(int i = 1; i <= 9; i++){
+            resultado = resultado*(i+1);
+            System.out.println(resultado);
+        }*/
+
+
+
+
+
+        /* 
+        //Questão 5
+        // Fatorial até 10
+        System.out.println("Fatoriais até 10!");
+        long resultado = 1;
+        System.out.println(resultado);
+        for(int i = 1; i <= 9; i++){
+            resultado = resultado*(i+1);
+            System.out.println(resultado);
         }
+        System.out.println("_____________________\n\n");
+
+
+        // Fatorial até 20
+        System.out.println("Fatoriais até 20!");
+        resultado = 1;
+        System.out.println(resultado);
+        for(int i = 1; i <= 19; i++){
+            resultado = resultado*(i+1);
+            System.out.println(resultado);
+        }
+        System.out.println("_____________________\n\n");
+
+        
+        // Fatorial até 30
+        System.out.println("Fatoriais até 30!");
+        resultado = 1;
+        System.out.println(resultado);
+        for(int i = 1; i <= 29; i++){
+            resultado = resultado*(i+1);
+            System.out.println(resultado);
+        }
+        System.out.println("_____________________\n\n");
+        */
+
+
+        //Questão 6
+        /* 
+        int primeiro = 0;
+        int segundo = 1;
+        System.out.println(primeiro);
+        System.out.println(segundo);
+        int atual = primeiro + segundo;
+        System.out.println(atual);
+        while(atual <= 100){
+            primeiro = segundo;
+            segundo = atual;
+            atual = primeiro + segundo;
+            System.out.println(atual);
+        }
+        */
+
+        
+
+        // Questão 7
+        /* 
+        int cima = 1;
+        int baixo = 1;
+        float resultado = 0;
+        
+        while(cima <= 99){
+            while(baixo <= 50){
+                resultado += (cima/baixo);
+                cima += 2;
+                baixo += 1;
+            }
+        }
+        System.out.println(cima);
+        System.out.println(baixo);
+        System.out.println(resultado);
+        */
+
+
+        
+
+        
+
+
+
 
 
 }
